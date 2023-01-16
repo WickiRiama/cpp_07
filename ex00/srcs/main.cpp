116894e26f860151dd14002f5bce3beb282c6041
Margaux Riant <mriant@student.42.fr>
2023-01-16 17:13:25 +0100

    Ex00 Swap OK
     Changes to be committed:
    	modified:   .gitignore
    	modified:   ex00/Makefile
    	deleted:    ex00/include/whatever.h
    	new file:   ex00/includes/whatever.h
    	modified:   ex00/srcs/main.cpp

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 17:05:45 by mriant            #+#    #+#             */
+/*   Updated: 2023/01/16 17:07:49 by mriant           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "whatever.h"
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
+}
